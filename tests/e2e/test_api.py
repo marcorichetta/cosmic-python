2d46deb98a519a49a5750f68346ab5dae38ec03e
Marco Richetta <marcorichetta@gmail.com>
2025-09-06 22:07:23 +0200

    refactor: use apiclient

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,7 +1,5 @@
 import pytest
-import requests
 
-from allocation import config
 from tests.e2e.api_client import APIClient
 from tests.random_refs import random_batchref, random_orderid, random_sku
 
@@ -23,20 +21,15 @@ def test_happy_path_returns_201_and_allocated_batch(api_client):
     api_client.post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
     api_client.post_to_add_batch(otherbatch, othersku, 100, None)
 
-    data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
-
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
+    r = api_client.post_to_allocate(random_orderid(), sku, 3)
 
     assert r.status_code == 201
     assert r.json()["ref"] == earlybatch
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_unhappy_path_returns_400_and_error_message():
+def test_unhappy_path_returns_400_and_error_message(api_client):
     unknown_sku, orderid = random_sku(), random_orderid()
-    data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
+    r = api_client.post_to_allocate(orderid, unknown_sku, 20)
     assert r.status_code == 400
     assert r.json()["message"] == f"Invalid sku {unknown_sku}"
