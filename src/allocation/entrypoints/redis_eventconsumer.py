4fb83485bd13507cbd04639175560cef37413137
Marco Richetta <marcorichetta@gmail.com>
2025-10-30 13:33:14 +0100

    fixes post merge

--- src/allocation/entrypoints/redis_eventconsumer.py
+++ src/allocation/entrypoints/redis_eventconsumer.py
@@ -1,10 +1,11 @@
 import json
 import logging
+
 import redis
 
 from allocation import config
-from allocation.domain import commands
 from allocation.adapters import orm
+from allocation.domain import commands
 from allocation.service_layer import messagebus, unit_of_work
 
 logger = logging.getLogger(__name__)
@@ -13,18 +14,21 @@ r = redis.Redis(**config.get_redis_host_and_port())
 
 
 def main():
+    """Executes on load and subscribes to Redis channel"""
     orm.start_mappers()
     pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe("change_batch_quantity")
 
-    for m in pubsub.listen():
-        handle_change_batch_quantity(m)
+    for message in pubsub.listen():
+        handle_change_batch_quantity(message)
 
 
-def handle_change_batch_quantity(m):
-    logging.debug("handling %s", m)
-    data = json.loads(m["data"])
-    cmd = commands.ChangeBatchQuantity(ref=data["batchref"], qty=data["qty"])
+def handle_change_batch_quantity(message):
+    logger.debug("handling %s", message)
+    data = json.loads(message["data"])
+    cmd = commands.ChangeBatchQuantity(
+        reference=data["batchref"], quantity=data["quantity"]
+    )
     messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
 
 
